9abb96923d2b9be9ccfe09cad3c2baaa7104bd52
Kimkungjong <rudwhd4474@gmail.com>
2021-01-04 15:32:00 +0900

    Updates

---
+++ precache-manifest.885c9696165d63219757c1c6ab87b44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab62f96de37ee21a54c145e9645bee2",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "096912b06cd1eae50586",
+    "url": "/react-todo-list/static/css/main.edc44fd5.chunk.css"
+  },
+  {
+    "revision": "17712b865522834f22b5",
+    "url": "/react-todo-list/static/js/2.c7653731.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react-todo-list/static/js/2.c7653731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096912b06cd1eae50586",
+    "url": "/react-todo-list/static/js/main.4ab91aca.chunk.js"
+  },
+  {
+    "revision": "80682d5f29ce18e87584",
+    "url": "/react-todo-list/static/js/runtime-main.28be1446.js"
+  }
+]);
